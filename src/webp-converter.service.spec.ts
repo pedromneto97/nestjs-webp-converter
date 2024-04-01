bffb3e14aa25a52ccc43314c59696acaf35781e0
Pedro Neto <pedromneto97@gmail.com>
2024-03-31 23:52:07 -0300

    test: add webp converter tests

--- src/webp-converter.service.spec.ts
+++ src/webp-converter.service.spec.ts
@@ -1,9 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { WebpConverterService } from './webp-converter.service';
+import { execFileSync } from 'child_process';
 import { existsSync } from 'fs';
+import path from 'path';
+import { InvalidOptionsError } from './options';
+import { WebpConverterService } from './webp-converter.service';
+
+jest.mock('child_process');
 
 describe('WebpConverterService', () => {
   let service: WebpConverterService;
+  const output = __dirname + '/../test/test.webp';
+  const input = __dirname + '/../test/test.jpg';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,13 +24,86 @@ describe('WebpConverterService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should convert an image to WebP format', async () => {
-    const output = __dirname + '/../test/test.webp';
+  const getExecutablePath = () => {
+    const vendorPath = '../vendor';
+    const paths = {
+      win32: {
+        x64: `${vendorPath}/win32/x64/cwebp.exe`,
+      },
+      linux: {
+        x64: `${vendorPath}/linux/x64/cwebp`,
+      },
+      darwin: {
+        arm64: `${vendorPath}/osx/arm64/cwebp`,
+        x64: `${vendorPath}/osx/x64/cwebp`,
+      },
+    };
 
-    await expect(
-      service.convert(__dirname + '/../test/test.jpg', output),
-    ).resolves.not.toThrow();
+    return path.join(__dirname, paths[process.platform][process.arch]);
+  };
+
+  it('should convert an image to WebP format', async () => {
+    await expect(service.convert(input, output)).resolves.not.toThrow();
 
     expect(existsSync(output)).toBe(true);
+
+    expect(execFileSync).toHaveBeenCalledWith(getExecutablePath(), [
+      input,
+      '-q',
+      '80',
+      '-mt',
+      '-m',
+      '6',
+      '-o',
+      output,
+    ]);
+  });
+
+  describe('mapOptions', () => {
+    describe('quality', () => {
+      it('should throw an error if the quality is less than zero', async () => {
+        await expect(() =>
+          service.convert(input, output, { quality: -1 }),
+        ).rejects.toEqual(new InvalidOptionsError('quality'));
+      });
+
+      it('should throw an error if the quality is greater than 100', async () => {
+        await expect(() =>
+          service.convert(input, output, { quality: 101 }),
+        ).rejects.toEqual(new InvalidOptionsError('quality'));
+      });
+    });
+
+    describe('compressionMethod', () => {
+      it('should throw an error if the compression method is less than zero', async () => {
+        await expect(() =>
+          service.convert(input, output, { compressionMethod: -1 }),
+        ).rejects.toEqual(new InvalidOptionsError('compressionMethod'));
+      });
+
+      it('should throw an error if the compression method is greater than 6', async () => {
+        await expect(() =>
+          service.convert(input, output, { compressionMethod: 7 }),
+        ).rejects.toEqual(new InvalidOptionsError('compressionMethod'));
+      });
+    });
+
+    it('should use lossless if provided', async () => {
+      await expect(
+        service.convert(input, output, { lossless: true }),
+      ).resolves.not.toThrow();
+
+      expect(execFileSync).toHaveBeenCalledWith(getExecutablePath(), [
+        input,
+        '-q',
+        '80',
+        '-mt',
+        '-lossless',
+        '-m',
+        '6',
+        '-o',
+        output,
+      ]);
+    });
   });
 });
