ae6800e9792c3eee733e2c65216f099e3f2d6c79
Pedro Neto <pedromneto97@gmail.com>
2024-03-31 23:13:29 -0300

    test: add convert test

--- src/webp-converter.service.spec.ts
+++ src/webp-converter.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WebpConverterService } from './webp-converter.service';
+import { existsSync } from 'fs';
 
 describe('WebpConverterService', () => {
   let service: WebpConverterService;
@@ -14,5 +15,15 @@ describe('WebpConverterService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should convert an image to WebP format', async () => {
+    const output = __dirname + '/../test/test.webp';
+
+    await expect(
+      service.convert(__dirname + '/../test/test.jpg', output),
+    ).resolves.not.toThrow();
+
+    expect(existsSync(output)).toBe(true);
   });
 });
